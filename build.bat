71a53883fc5cbbf1f8bc856d92944b26a1febe75
David <dkanekanian1@gmail.com>
2020-05-09 17:04:41 +0100

    Make build batch file use local node modules

--- build.bat
+++ build.bat
@@ -4,10 +4,10 @@ REM Create the build directory if not already there.
 if not exist build (mkdir build)
 
 REM Bundle the javascript to include three.js and other modules
-call browserify src/tower.js > build/bundle.js
+call ./node_modules/.bin/browserify src/tower.js > build/bundle.js
 
 REM Build the pug to html files
-call pug src/ --out build/
+call ./node_modules/.bin/pug src/ --out build/
 
 REM Build the sass to css files
-call sass src/:build/
+call ./node_modules/.bin/sass src/:build/
