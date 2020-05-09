9cc07e665c55adc2aa3433699c1d9b145eb8a945
David <dkanekanian1@gmail.com>
2020-05-09 14:52:09 +0100

    Initial commit - empty index page

---
+++ build.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM Create the build directory if not already there.
+if not exist build (mkdir build)
+
+REM Bundle the javascript to include three.js and other modules
+call browserify src/tower.js > build/bundle1.js
+
+REM Build the pug file
+call pug src/ --out build/
---
+++ src/tower.js
@@ -0,0 +1,3 @@
+var THREE = require("three");
+
+console.log(new THREE.Vector3(1, 2, 3));
