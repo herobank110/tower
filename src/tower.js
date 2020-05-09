3ca238217e661c5752e91e13829655afbebe9ce8
David <dkanekanian1@gmail.com>
2020-05-09 18:49:06 +0100

    Add node palette opening detection

--- src/tower.js
+++ src/tower.js
@@ -2,9 +2,12 @@ var THREE = require("three");
 
 var model = {
     sceneInteract: {
-        panStarted: false,
-        zoomStarted: false,
-        trueCameraZ: 5.
+        bPanStarted: false,
+        bZoomStarted: false,
+        trueCameraZ: 5.,
+        bJustMovedCamera: false,
+        bJustReleasedPan: false,
+        bMovedCameraSinceInteract: false
     }
 };
 
@@ -28,44 +31,47 @@ camera.position.z = Math.floor(model.sceneInteract.trueCameraZ);
 var animate = function () {
     requestAnimationFrame(animate);
 
+    if (!model.sceneInteract.bMovedCameraSinceInteract
+        && model.sceneInteract.bJustReleasedPan
+    ) {
+        // Open the node palette.
+        console.log("Opening node palette");
+    }
+
     renderer.render(scene, camera);
+
+    // Reset per-frame state.
+    model.sceneInteract.bJustMovedCamera = false;
+    model.sceneInteract.bJustReleasedPan = false;
 };
 
 animate();
 
-function screenToWorld(_screenPos)
-{
-    var worldPos = _screenPos.clone();
-    // worldPos.x = worldPos.x / CANVAS_SIZE.x/2 - 1;
-    // worldPos.y = - worldPos.y / CANVAS_SIZE.y/2 + 1;
-    worldPos.unproject( camera );
-    return worldPos;                    
-}
-
-function worldToScreen(_worldPos)
-{
-    var screenPos = _worldPos.clone();
-    projector.projectVector( screenPos, camera );
-    screenPos.x = ( screenPos.x + 1 ) * windowHalfX;
-    screenPos.y = ( - screenPos.y + 1) * windowHalfY;
-    return screenPos;
-}
-
 window.addEventListener("mousemove", function (event) {
     var cameraDist = camera.position.z;
 
-    if (model.sceneInteract.panStarted) {
+    if (model.sceneInteract.bPanStarted) {
         camera.position.x -= event.movementX * 0.0026 * cameraDist;
         camera.position.y += event.movementY * 0.0026 * cameraDist;
-    } else if (model.sceneInteract.zoomStarted) {
+
+        // Notify the input system that the camera was moved.
+        model.sceneInteract.bJustMovedCamera = true;
+        model.sceneInteract.bMovedCameraSinceInteract = true;
+
+    } else if (model.sceneInteract.bZoomStarted) {
         // Set the true float zoom level in the model but clamp to a
         // 'ratchet' system for the camera position.
         var oldDist = model.sceneInteract.trueCameraZ;
         var newDist = oldDist - event.movementX * 0.0018 * cameraDist;
         newDist = THREE.MathUtils.clamp(newDist, 2, 20);
         model.sceneInteract.trueCameraZ = newDist;
+
         // Clamp the camera to a 'grid' system.
         camera.position.z = Math.floor(newDist);
+
+        // Notify the input system that the camera was moved.
+        model.sceneInteract.bJustMovedCamera = true;
+        model.sceneInteract.bMovedCameraSinceInteract = true;
     }
 });
 
@@ -76,10 +82,12 @@ towerCanvas.addEventListener("mousedown", function (event) {
     switch (event.which) {
     case 1: break;
     case 2:
-        model.sceneInteract.zoomStarted = true;
+        model.sceneInteract.bZoomStarted = true;
+        model.sceneInteract.bMovedCameraSinceInteract = false;
         break;
     case 3:
-        model.sceneInteract.panStarted = true;
+        model.sceneInteract.bPanStarted = true;
+        model.sceneInteract.bMovedCameraSinceInteract = false;
         break;
     };
 });
@@ -90,10 +98,11 @@ window.addEventListener("mouseup", function (event) {
     switch (event.which) {
     case 1: break;
     case 2:
-        model.sceneInteract.zoomStarted = false;
+        model.sceneInteract.bZoomStarted = false;
         break;
     case 3:
-        model.sceneInteract.panStarted = false;
+        model.sceneInteract.bPanStarted = false;
+        model.sceneInteract.bJustReleasedPan = true;
         break;
     };
 });
