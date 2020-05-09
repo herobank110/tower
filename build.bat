2914dbf25d847fdf51b837e6c5566f566f7caa0e
David <dkanekanian1@gmail.com>
2020-05-09 16:18:37 +0100

    Add sass stylesheet support

--- build.bat
+++ build.bat
@@ -6,5 +6,8 @@ if not exist build (mkdir build)
 REM Bundle the javascript to include three.js and other modules
 call browserify src/tower.js > build/bundle.js
 
-REM Build the pug file
+REM Build the pug to html files
 call pug src/ --out build/
+
+REM Build the sass to css files
+call sass src/:build/
