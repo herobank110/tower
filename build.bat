42a19da13e9d0615b9bb71890090b0363604f885
herobank110 <dkanekanian1@gmail.com>
2020-05-09 14:59:23 +0100

    Fix build script wrong bundle filename

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ REM Create the build directory if not already there.
 if not exist build (mkdir build)
 
 REM Bundle the javascript to include three.js and other modules
-call browserify src/tower.js > build/bundle1.js
+call browserify src/tower.js > build/bundle.js
 
 REM Build the pug file
 call pug src/ --out build/
