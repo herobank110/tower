92b2c0ea40df3d80ab31dd8997f996b0b54cc2db
David <dkanekanian1@gmail.com>
2020-05-09 17:56:03 +0100

    Disable scrolling on body

--- src/tower.js
+++ src/tower.js
@@ -1,11 +1,19 @@
 var THREE = require("three");
 
+var model = {
+    sceneInteract: {
+        panStarted: false,
+        zoomStarted: false
+    }
+};
+
 const CANVAS_SIZE = new THREE.Vector2(800, 600);
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, CANVAS_SIZE.x / CANVAS_SIZE.y, 0.1, 1000);
+var towerCanvas = document.querySelector("#tower-canvas");
 
-var renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("#tower-canvas") });
+var renderer = new THREE.WebGLRenderer({ canvas: towerCanvas });
 renderer.setSize(CANVAS_SIZE.x, CANVAS_SIZE.y);
 renderer.setClearColor(0xbbbbbb);
 
@@ -23,8 +31,38 @@ var animate = function () {
 
 animate();
 
-
 window.addEventListener("mousemove", function (event) {
-    camera.position.x -= event.movementX * 0.012;
-    camera.position.y += event.movementY * 0.012;
-})
+    if (model.sceneInteract.panStarted) {
+        camera.position.x -= event.movementX * 0.012;
+        camera.position.y += event.movementY * 0.012;
+    }
+});
+
+// Don't show the normal right click menu on the canvas.
+towerCanvas.addEventListener('contextmenu', event => event.preventDefault());
+
+towerCanvas.addEventListener("mousedown", function (event) {
+    switch (event.which) {
+    case 1: break;
+    case 2:
+        model.sceneInteract.zoomStarted = true;
+        break;
+    case 3:
+        model.sceneInteract.panStarted = true;
+        break;
+    };
+});
+
+towerCanvas.addEventListener("mouseup", function (event) {
+    event.preventDefault();
+    switch (event.which) {
+    case 1: break;
+    case 2:
+        model.sceneInteract.zoomStarted = false;
+        break;
+    case 3:
+        model.sceneInteract.panStarted = false;
+        break;
+    };
+});
+
